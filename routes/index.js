28b650aacbf54094ea78ed694b9471f58689fa8b
Tyler Zhu <tylerzhu@Tylers-MacBook-Pro.local>
2018-07-28 19:14:05 -0700

    dumb shit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const request = require('request')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  const url = "https://api.globalgateway.io/verifications/v1/verify";
+  request.get(url, (err, response, body) => {
+    if(err) {console.error(err)}
+    body = JSON.parse(body);
+    console.log(body);
+
+  res.render('index', {title: 'Express'});
+});
 });
 
 module.exports = router;
