7a42b57b9ecdd6bdf0bbfd20119db88ec168a2c8
Eric Frankel <eric-frankel@cox.net>
2018-08-09 13:54:41 -0700

    redefining tokens and fixed migration

--- foodcoin_token/migrations/1_initial_migration.js
+++ foodcoin_token/migrations/1_initial_migration.js
@@ -1,9 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
-var FoodToken = artifacts.require("./FoodToken.sol");
-var Purchase = artifacts.require("./FoodToken.sol");
+const Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(Migrations);
-  deployer.deploy(FoodToken, accounts[0]);
-  deployer.deploy(Purchase, accounts[1]);
 };
